c8a3ddad6e0a5d87d419c393a16bccda5beefc6e
emonibnsalim <emonibnsali@gmail.com>
2023-07-12 10:31:01 +0600

    [done] redux module 21 done

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+
+import { decrement, increment, incrementByAmount } from "./redux/features/counter/counterSlice"
+import { useAppDispatch, useAppSelector } from "./redux/hook"
+
+function App() {
+  const {count}  = useAppSelector(state => state.counter)
+  const dispatch = useAppDispatch()
+
+  return (
+    <div>
+      <div className="flex gap-6">
+        <button className="border-2 border-green-500 rounded-md px-2 py-3" onClick={() => dispatch(increment())}>
+          Increment
+        </button>
+        <div>{count}</div>
+        <button className="border-2 border-red-500 rounded-md px-2 py-3" onClick={() => dispatch(decrement())}>
+          Decrement
+        </button>
+        <button className="border-2 border-red-500 rounded-md px-2 py-3" onClick={() => dispatch(incrementByAmount(50))}>
+          amount dobile
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/redux/store.ts
@@ -0,0 +1,17 @@
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import counterReducer from './features/counter/counterSlice';
+import logger from './middleware/logger';
+
+
+export const store = configureStore({
+    reducer: {
+        counter: counterReducer
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
+})
+
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+
+export type AppDispatch = typeof store.dispatch
